dd28de9e1922c18f1313045656ea185d7da4084e
Zhe Fu <fuzhe@yahoo.com>
2017-11-15 15:28:45 -0800

    first upload

---
+++ current/train_1.py
@@ -0,0 +1,24 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='1'
+
+'''
+from net.common import *
+from net.utility.file import *
+from net.processing.boxes import *
+from net.processing.boxes3d import *
+from net.utility.draw import *
+
+from dummynet import *
+from data import *
+
+from net.rpn_loss_op import *
+from net.rcnn_loss_op import *
+from net.rpn_target_op import make_bases, make_anchors, rpn_target
+from net.rcnn_target_op import rcnn_target
+
+from net.rpn_nms_op     import draw_rpn_nms, draw_rpn
+from net.rcnn_nms_op    import rcnn_nms, draw_rcnn_nms, draw_rcnn
+from net.rpn_target_op  import draw_rpn_gt, draw_rpn_targets, draw_rpn_labels
+from net.rcnn_target_op import draw_rcnn_targets, draw_rcnn_labels
+'''
+
